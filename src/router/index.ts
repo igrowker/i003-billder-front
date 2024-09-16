63d336c5d5eb9983d792b95aa61336302075174f
Mayra Sanchez <lilimayra@gmail.com>
2024-09-16 19:39:17 +0200

    feat: #20 formulario login

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/',
+      name: 'login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/LoginView.vue')
     }
   ]
 })
---
+++ src/views/LoginView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex flex-col sm:flex-row-reverse w-full">
+    <div class="flex justify-center items-center sm:w-1/2">
+      <img alt="img" class="w-full h-auto" src="@/assets/img/imago_billder.png" />
+    </div>
+
+    <form class="bg-white w-full sm:px-11 sm:w-1/2 flex flex-col justify-center">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+          Accede a tu cuenta
+        </label>
+        <input
+          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="E-mail"
+        />
+      </div>
+      <div class="mb-6">
+        <input
+          class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          placeholder="Contraseña"
+        />
+        <!--  <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
+      </div>
+      <div class="flex items-center justify-end">
+        <a
+          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="#"
+        >
+          Olvidaste tu contraseña?
+        </a>
+      </div>
+
+      <div class="flex flex-col">
+        <button
+          class="bg-yellow-600 hover:bg-blue-900 text-white font-bold my-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Iniciar sesión
+        </button>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Registrarse
+        </button>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold my-2 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Acceder con Google
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

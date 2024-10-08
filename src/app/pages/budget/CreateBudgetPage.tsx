39488f1205870998997202f7325ad46aeb4715d3
Mayra Sanchez <lilimayra@gmail.com>
2024-10-08 12:12:39 +0200

    calculo de presupuesto

--- src/app/pages/budget/CreateBudgetPage.tsx
+++ src/app/pages/budget/CreateBudgetPage.tsx
@@ -1,84 +1,79 @@
-import { BudgetCalcTab, BudgetConfirmDataTab, BudgetViewDocumentTab } from "@/app/components"
-import { ReturnLayout } from "@/layouts/ReturnLayout"
-import { ReusableButton } from "@/ui/components"
-import { useState, useTransition } from "react"
-import { useNavigate } from "react-router-dom"
-
+import {
+  BudgetCalcTab,
+  BudgetConfirmDataTab,
+  BudgetViewDocumentTab,
+} from "@/app/components";
+import { ReturnLayout } from "@/layouts/ReturnLayout";
+import { ReusableButton } from "@/ui/components";
+import { useState, useTransition } from "react";
+import { useNavigate } from "react-router-dom";
 
 enum CreateBudgetTabs {
   Initial = 1,
   Payment = 2,
-  End = 3
+  End = 3,
 }
 const phases = [
   {
     phase: 1,
-    title: 'Confirmá los datos',
-    component: <BudgetConfirmDataTab />
+    title: "Confirmá los datos",
+    component: <BudgetConfirmDataTab />,
   },
   {
     phase: 2,
-    title: 'Realizá el cálculo',
-    component: <BudgetCalcTab />
+    title: "Realizá el cálculo",
+    component: <BudgetCalcTab />,
   },
   {
     phase: 3,
-    title: '¡Todo listo!',
-    component: <BudgetViewDocumentTab />
+    title: "¡Todo listo!",
+    component: <BudgetViewDocumentTab />,
   },
-
-]
-
+];
 
 export const CreateBudgetPage = () => {
-
   const navigate = useNavigate();
-  const [tab, setTab] = useState(phases.find((phase) => phase.phase == CreateBudgetTabs.Initial)!);
-  const [isPending, startTransition] = useTransition()
+  const [tab, setTab] = useState(
+    phases.find(phase => phase.phase == CreateBudgetTabs.Initial)!
+  );
+  const [isPending, startTransition] = useTransition();
 
   const handleChangeTab = () => {
     if (tab.phase == CreateBudgetTabs.End) return;
     startTransition(() => {
-      setTab(phases[tab.phase])
-
+      setTab(phases[tab.phase]);
     });
     if (isPending) return;
-  }
+  };
   const handleGoBack = () => {
-
     if (tab.phase === CreateBudgetTabs.Initial) {
       startTransition(() => {
-        setTab(phases.find((phase) => phase.phase == CreateBudgetTabs.Initial)!);
-        navigate(-1)
-
-      })
+        setTab(phases.find(phase => phase.phase == CreateBudgetTabs.Initial)!);
+        navigate(-1);
+      });
       return;
-    };
+    }
     startTransition(() => {
-      setTab(phases.find(phase => phase.phase === tab.phase - 1)!)
-
-    })
-  }
+      setTab(phases.find(phase => phase.phase === tab.phase - 1)!);
+    });
+  };
 
   return (
     <ReturnLayout
       returnFunction={() => handleGoBack()}
       title="Crear presupuesto"
-
     >
       <div className="">
-        <h4 className="font-medium text-customOrange text-xl mt-4 mb-2">Paso {tab.phase}: {tab.title}</h4>
+        <h4 className="font-medium text-customOrange text-xl mt-4 mb-2">
+          Paso {tab.phase}: {tab.title}
+        </h4>
         {tab.component}
       </div>
-      {
-        (tab.phase !== CreateBudgetTabs.End)
-        && (
-          <ReusableButton onClick={handleChangeTab} className="mt-8">
-            Continuar
-          </ReusableButton>
-
-        )
-      }
+      {tab.phase !== CreateBudgetTabs.End && (
+        <ReusableButton onClick={handleChangeTab} className="mt-8">
+          Continuar
+        </ReusableButton>
+      )}
     </ReturnLayout>
-  )
-}
+  );
+};
--- src/ui/components/modals/Modal.tsx
+++ src/ui/components/modals/Modal.tsx
@@ -1,16 +1,37 @@
+import React from "react";
+import { CloseIcon } from "@/assets/icons/";
+
 interface ModalProps {
   isOpen: boolean;
   title?: string;
   footer?: React.ReactNode;
   children: React.ReactNode;
+  onClose: () => void;
+  showCloseButton?: boolean; // Propiedad opcional para mostrar el botón de cierre
 }
 
-export const Modal = ({ isOpen, title, footer, children }: ModalProps) => {
+export const Modal = ({
+  isOpen,
+  title,
+  footer,
+  children,
+  onClose,
+  showCloseButton = true,
+}: ModalProps) => {
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-800 bg-opacity-70">
-      <div className="bg-white rounded-lg shadow-lg p-4 w-full max-w-xs">
+      <div className="bg-white rounded-lg shadow-lg p-4 w-full max-w-xs relative">
+        {showCloseButton && (
+          <button
+            className="absolute top-2 right-2 p-2 text-gray-600 hover:text-gray-900"
+            onClick={onClose}
+            aria-label="Cerrar"
+          >
+            <CloseIcon />
+          </button>
+        )}
         {title && <h2 className="text-lg font-semibold mb-2">{title}</h2>}
         <div className="mb-4">{children}</div>
         <div className="flex justify-end">{footer ? footer : <></>}</div>

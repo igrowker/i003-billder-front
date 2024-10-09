62072e793706c7b4aac616b009bb4ba6ed5f388f
joaquin feola <joacoarielfeola@gmail.com>
2024-10-09 10:24:14 -0300

    appi for production added

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const httpClient = axios.create({
-    baseURL: "http://localhost:5048/api",
+    baseURL: import.meta.env.DEV ? "http://localhost:5048/api" : 'https://5f4e-2803-3480-3886-b700-dc53-3aa4-875e-59f.ngrok-free.app',
 });
 
 

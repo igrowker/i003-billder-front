fedf32eef9c3dd09105c2efd95e9f1d4213c70e7
joaquin feola <joacoarielfeola@gmail.com>
2024-10-09 11:37:16 -0300

    ""

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const httpClient = axios.create({
-    baseURL: !import.meta.env.DEV ? "http://localhost:5048/api" : ' https://9cd3-2803-3480-3886-b700-dc53-3aa4-875e-59f.ngrok-free.app/api',
+    baseURL: import.meta.env.DEV ? "http://localhost:5048/api" : ' https://9cd3-2803-3480-3886-b700-dc53-3aa4-875e-59f.ngrok-free.app/api',
     headers: {
         'ngrok-skip-browser-warning': 'any-value'
     }
